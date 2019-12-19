3b91208cb80829f89a67c111aedefed65dd390dd
hu <hu@qq.com>
2019-12-19 21:00:58 +0800

    class

---
+++ shiyaneleven.java
@@ -0,0 +1,28 @@
+package com.classbook.chapter.work;
+
+import java.io.*;
+
+public class TxtW {
+    public static void main(String[] args) {
+        try {
+            BufferedReader stdin =
+                    new BufferedReader(
+                            new InputStreamReader(System.in));
+            System.out.println("输入 内容1");
+            String line = stdin.readLine();
+            System.out.println("输入 内容2");
+            String line0 = stdin.readLine();
+            BufferedWriter fout =
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    new FileOutputStream("Userinfomation.txt")));
+            //   fout.write();
+            fout.write(line);
+            fout.write(line0);
+            fout.close();
+        } catch (IOException e) {
+            System.out.println("...");
+        }
+
+    }
+}
